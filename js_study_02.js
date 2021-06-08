b7f58e8937f4fabaf1765c0ae71d7431b258c3db
jihunHong <>
2021-06-08 07:37:34 +0000

    inital commit

---
+++ js_study_02.js
@@ -0,0 +1,31 @@
+//숫자
+var a=Math.pow(3,2)
+console.log(a)
+var b=Math.round(10,6)
+console.log(b)
+var c=Math.ceil(10.2)
+var d=Math.floor(10.6)
+var e=Math.sqrt(9)
+console.log(c,d,e)
+var f=Math.random()//0부터 1까지 랜덤수 
+console.log(f)
+var g=Math.round(100*Math.random());
+console.log(g)
+console.log("\n")
+
+//문자
+console.log("hello")//따옴표 사이에 있으면 문자
+console.log("hello'")
+console.log('hel\'lo')//이스케이프 문자
+console.log(typeof(1))
+console.log(typeof("hi!"))
+console.log("\n")
+
+//문자의 연산
+console.log("hello"+"world")
+console.log(1+1)
+console.log("1"+"1")
+console.log("helloworld".length)
+//생활코딩 사전 확인!
+console.log("helloworld".indexOf("h"))
+console.log("helloworld".indexOf("e"))
