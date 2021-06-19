bbfb161293fe03513e7d15b09f8e06fbc55be58d
jihunHong <48124955+HONG-C@users.noreply.github.com>
2021-06-19 14:37:48 +0000

    배열의 추가,제거,정렬 등 내용 추가.c++과 파이썬과 비슷하지만 다른점이 많아 자주 복습 필요할듯

--- js_study_07.js
+++ js_study_07.js
@@ -36,3 +36,38 @@ for(i = 0; i < new_members.length; i++){
 //document.write('<br />');//html에서의 줄바꿈!
 //(배열 이름).length:배열에 담긴 값의 개수 반환 
 // (배열이름)[i].toUpperCase():(배열이름)[i]에 담긴 문자를 대문자로 변환
+
+
+//4.배열의 조작-추가 
+console.log("4.배열의 조작-추가")
+var arr=[1,2,3,4,5];
+console.log(arr.length)
+arr.push(6);//배열의 값 추가 
+console.log(arr)
+arr=arr.concat(['7','8']);//배열의 값 추가(복수)
+console.log(arr)
+arr.unshift('0');//가장 앞부분에 요소 추가 
+console.log(arr)
+arr.splice(1,1,'a');//생활코딩에 자세한 인자 내용 나와있으니 확인!
+//array.splice(index,howmany,element)
+//index:특정 배열 위치
+//howmany:index에서부터 제거될 원소의 수.0이면 아무것도 삭제 안함
+//element:index와 howmany사이 추가될 값 
+//추가적으로 삭제될 값 리턴함!
+//다 외우기보단 검색하면서 그때그때 사용법 확인!
+console.log(arr)
+
+//5.배열의 조작-제거,정렬
+console.log("5.배열의 조작-제거,정렬")
+var li=['a','f','g','d','c','k','e'];
+li.shift();//맨 앞에 요소 삭제
+console.log(li)
+li.pop();//맨 끝에 요소 삭제
+console.log(li)
+li.sort();
+console.log(li)
+li.reverse();
+console.log(li)
+
+//원하는 방식으로 정렬하고 싶을땐 생활코딩에서 데이터 값을 찾아서 하기!
+//array.sort(sortfunc):요기서 sortfunc을 정의하여 입맛대로 정렬가능
